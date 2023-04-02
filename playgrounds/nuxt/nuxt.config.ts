3a489d2c1099734756300706bfc974928e03249e
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-02 10:56:57 +0200

    chore: fix type

--- playgrounds/nuxt/nuxt.config.ts
+++ playgrounds/nuxt/nuxt.config.ts
@@ -1,6 +1,8 @@
 import { transformLazyShow } from '../../src'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore ignore
 export default defineNuxtConfig({
   vue: {
     compilerOptions: {
